cfcec6262679ab4ca0ad5642659aca5824dd8456
KumarRajnish2003 <merajnish14.1.2003@gmail.com>
2025-07-11 10:10:27 +0530

    home page changed

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,59 +1,54 @@
 package pageObjects;
-
+ 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-
+ 
 public class HomePage extends BasePage {
-
+ 
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
-
+ 
 	JavascriptExecutor js = (JavascriptExecutor) driver;
 	Actions action = new Actions(driver);
-
+ 
 	@FindBy(id = "forum_login_title_lg")
 	WebElement LoginAndMore;
-
+ 
 	@FindBy(xpath = "//*[@id=\"headerNewVNavWrap\"]/nav/ul/li[5]/span")
 	WebElement More;
-
+ 
 	@FindBy(linkText = "Used Cars")
 	WebElement usedCar;
-
-	@FindBy(xpath = "//*[@id=\"bike-tabs\"]/li[3]")
+ 
+	@FindBy(xpath = "//*[@class='upcoming-bike-tab']")
 	WebElement upcomingBikes;
-
-	@FindBy(xpath = "//*[@id=\"zw-cmnSilder\"]/div[2]/a")
+ 
+	@FindBy(linkText = "Upcoming Bikes")
 	WebElement allUpcomingBikes;
-
+ 
 	public void ClickLoginAndMore() {
 		LoginAndMore.click();
 	}
-	
-	public void hoverMore() {
-		Actions action=new Actions(driver);
-		action.moveToElement(More);
-	}
-
-	public void clickUsedCars() {
-
+ 
+	public void clickUsedCars(WebDriver driver) {
+ 
 		action.moveToElement(More).perform();
 		usedCar.click();
 	}
-
+ 
 	public void clickonUpcomingBike() {
-
-		js.executeScript("arguments[0].scrollIntoView(true);", upcomingBikes);
-		upcomingBikes.click();
+ 
+		js.executeScript("arguments[0].scrollIntoView(true)", upcomingBikes);
+		js.executeScript("arguments[0].click()",upcomingBikes);
 	}
-
+ 
 	public void clickonAllUpcomingBike() {
-		js.executeScript("arguments[0].scrollIntoView(true);", allUpcomingBikes);
-		allUpcomingBikes.click();
+		js.executeScript("arguments[0].scrollIntoView(true)", allUpcomingBikes);
+		js.executeScript("arguments[0].click()",allUpcomingBikes);
 	}
-
+ 
 }

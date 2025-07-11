e40fc4bba7e007787da6a3365f455def3368eaea
KumarRajnish2003 <merajnish14.1.2003@gmail.com>
2025-07-11 11:18:32 +0530

    hondaupcoming,upcomingbike,TS_upcoming midified

--- src/test/java/pageObjects/UpcomingBikePage.java
+++ src/test/java/pageObjects/UpcomingBikePage.java
@@ -1,5 +1,21 @@
 package pageObjects;
+ 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+ 
+public class UpcomingBikePage extends BasePage {
+	public UpcomingBikePage(WebDriver driver) {
+		super(driver);
+	}
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	@FindBy(linkText="Honda")
+	WebElement honda;
+	public void clickOnHonda() {
+		js.executeScript("arguments[0].scrollIntoView(true)", honda);
+		js.executeScript("arguments[0].click()",honda);
+	}
 
-public class UpcomingBikePage {
-
+ 
 }
